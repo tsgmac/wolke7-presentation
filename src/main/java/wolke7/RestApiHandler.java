91dcda6c4d72fb1c671e42a493d53824ace1f1ff
tsgmac <tsgmac@gmail.com>
2015-04-28 23:31:44 +0200

    @RequestBody instead of @RequestParam

--- src/main/java/wolke7/RestApiHandler.java
+++ src/main/java/wolke7/RestApiHandler.java
@@ -16,9 +16,9 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -134,7 +134,8 @@ public class RestApiHandler extends AbstractRestHandler {
 	@RequestMapping(value = "cluster-experiments", method = RequestMethod.POST)	
 	@ResponseStatus(HttpStatus.CREATED)
 	public @ResponseBody Response createClusterExperiment(
-			@RequestParam(value = "input") String input)
+			@RequestBody String input)
+//			@RequestParam(value = "input") String input)
 			throws URISyntaxException, KeyManagementException,
 			NoSuchAlgorithmException, IOException {
 		return (new ClusterExperimentAPI()).createClusterExperiment(input);
